2309763ec9861afa988046994f12dcd0cf9ad4c0
newcomet-lab <newcometcto729@gmail.com>
2021-12-07 08:16:21 -0800

    sidebar js customized

--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as SimpleBar from "simplebar";
 
 @Component({
   selector: 'app-side-nav',
@@ -10,6 +11,50 @@ export class SideNavComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    document.addEventListener("DOMContentLoaded", () => this.initialize());
+  }
+
+  initialize = () => {
+    this.initializeSimplebar();
+    this.initializeSidebarCollapse();
+  }
+  
+  initializeSimplebar = () => {
+    const simplebarElement : any = document.getElementsByClassName("js-simplebar")[0];
+  
+    if (simplebarElement) {
+      const simplebarInstance = new SimpleBar(simplebarElement);
+  
+      /* Recalculate simplebar on sidebar dropdown toggle */
+      const sidebarDropdowns = document.querySelectorAll(".js-sidebar [data-bs-parent]");
+  
+      sidebarDropdowns.forEach(link => {
+        link.addEventListener("shown.bs.collapse", () => {
+          simplebarInstance.recalculate();
+        });
+        link.addEventListener("hidden.bs.collapse", () => {
+          simplebarInstance.recalculate();
+        });
+      });
+    }
+  }
+  
+  initializeSidebarCollapse = () => {
+    const sidebarElement = document.getElementsByClassName("js-sidebar")[0];
+    const sidebarToggleElement : any = document.getElementsByClassName("js-sidebar-toggle")[0];
+    if (sidebarElement && sidebarToggleElement) {
+      sidebarToggleElement.addEventListener("click", () => {
+        sidebarElement.classList.toggle("collapsed");
+        sidebarElement.addEventListener("transitionend", () => {
+          window.dispatchEvent(new Event("resize"));
+        });
+      });
+    } else {
+      sidebarToggleElement.style.visibility = 'hidden';
+    }
   }
 
 }
