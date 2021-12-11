8a02d8b250afa9ae3875d911ae2a4ec0a87dd33c
newcomet-lab <newcometcto729@gmail.com>
2021-12-10 21:13:31 -0800

    candle chart

--- src/app/components/trade/trade.module.ts
+++ src/app/components/trade/trade.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { HttpClientModule  } from '@angular/common/http';
 
 import { TradeRoutingModule } from './trade-routing.module';
 import { TradeComponent } from './trade.component';
 import { TradeIndexComponent } from './trade-index/trade-index.component';
 import { SharedModule } from '../shared/shared.module';
+
+import { TradeHistoryService } from '../../services/trade-history.service';
+import { SocketService } from '../../services/socket.service';
 
 
 @NgModule({
@@ -20,7 +24,12 @@ import { SharedModule } from '../shared/shared.module';
     ReactiveFormsModule,
     TradeRoutingModule,
     SharedModule,
-    NgApexchartsModule
+    NgApexchartsModule,
+    HttpClientModule
+  ],
+  providers: [
+    TradeHistoryService,
+    SocketService
   ]
 })
 export class TradeModule { }
